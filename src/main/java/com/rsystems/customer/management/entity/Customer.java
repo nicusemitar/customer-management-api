72a688b2fd1fd2a9eeba159bf501fe79ff7bf6b4
nicolae.semitar <nicolae.semitar@eu.rsystems.com>
2024-07-16 18:05:13 +0300

    NEM: added Customer Entity

---
+++ src/main/java/com/rsystems/customer/management/entity/Customer.java
@@ -0,0 +1,45 @@
+package com.rsystems.customer.management.entity;
+
+public class Customer {
+    private Long id;
+    private String name;
+    private String phoneNumber;
+
+    public Customer() {
+    }
+
+    public Customer(Long id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return id + "," + name + "," + phoneNumber;
+    }
+}

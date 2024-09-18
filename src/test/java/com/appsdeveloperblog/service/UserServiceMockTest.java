d9fb56d4e9c0c6080b2cafce3fca805701f109a5
nicolae.semitar <nicolae.semitar@eu.rsystems.com>
2024-09-18 18:19:12 +0300

    NEM: Test class for User functionality

---
+++ src/test/java/com/appsdeveloperblog/service/UserServiceMockTest.java
@@ -0,0 +1,104 @@
+package com.appsdeveloperblog.service;
+
+import com.appsdeveloperblog.exception.EmailNotificationServiceException;
+import com.appsdeveloperblog.exception.UserServiceException;
+import com.appsdeveloperblog.model.User;
+import com.appsdeveloperblog.repo.UserRepositoryImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceMockTest {
+
+    UserServiceImpl userService;
+    @Mock
+    UserRepositoryImpl userRepository;
+
+    @Mock
+    EmailNotificationServiceImpl emailNotificationService;
+
+    User user;
+
+    @BeforeEach
+    void init() {
+
+        userService = new UserServiceImpl(userRepository, emailNotificationService);
+        user = new User("1", "John", "Last", "john@mail.com");
+
+    }
+
+    @DisplayName("Test create a new user")
+    @Test
+    void testCreateNewUser() throws UserServiceException {
+        //Arrange
+        when(userRepository.save(user)).thenReturn(true);
+
+        User userCreated = userService.createUser(user);
+        verify(userRepository, times(1)).save(user);
+        assertEquals("John", userCreated.getFirstName());
+
+    }
+
+    @Disabled
+    @DisplayName("Test create a new user with Exception")
+    @Test
+    void testCreateNewUser_ThrowsException() {
+        //Arrange
+        when(userRepository.save(user)).thenThrow(UserServiceException.class);
+
+        //Act & Assert
+        assertThrows(UserServiceException.class,
+                () -> userService.createUser(user));
+
+    }
+
+    @Test
+    void testEmailService_ThrowsException() {
+
+        //Arrange
+        when(userRepository.save(user)).thenReturn(true);
+        doThrow(EmailNotificationServiceException.class)
+                .when(emailNotificationService)
+                .scheduleEmailConfirmation(user);
+
+        //Act & Assert
+        assertThrows(UserServiceException.class, () ->
+                userService.createUser(user));
+    }
+
+    @Test
+    void testEmailService_Invocation() throws UserServiceException {
+
+        //Arrange
+        when(userRepository.save(user)).thenReturn(true);
+        doNothing().when(emailNotificationService).scheduleEmailConfirmation(user);
+
+        //Act & Assert
+        userService.createUser(user);
+        verify(emailNotificationService, times(1)).scheduleEmailConfirmation(user);
+    }
+
+    @Test
+    void testEmailService_doCallRealMethod() throws UserServiceException {
+
+        //Arrange
+        when(userRepository.save(user)).thenReturn(true);
+
+        doCallRealMethod().when(emailNotificationService).scheduleEmailConfirmation(user);
+
+        //Act & Assert
+        userService.createUser(user);
+        verify(emailNotificationService, times(1)).scheduleEmailConfirmation(user);
+    }
+
+
+}

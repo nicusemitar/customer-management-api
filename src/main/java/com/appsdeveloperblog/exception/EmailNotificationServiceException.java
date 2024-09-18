34108e522af613eb49770242892155337e7e7f44
nicolae.semitar <nicolae.semitar@eu.rsystems.com>
2024-09-18 17:52:09 +0300

    NEM: Added exceptions classes for test purposes

---
+++ src/main/java/com/appsdeveloperblog/exception/EmailNotificationServiceException.java
@@ -0,0 +1,9 @@
+package com.appsdeveloperblog.exception;
+
+public class EmailNotificationServiceException extends RuntimeException {
+    public EmailNotificationServiceException(String message) {
+        super(message);
+    }
+}
+
+
---
+++ src/main/java/com/appsdeveloperblog/exception/UserServiceException.java
@@ -0,0 +1,7 @@
+package com.appsdeveloperblog.exception;
+
+public class UserServiceException extends Throwable {
+    public UserServiceException(String message) {
+        super(message);
+    }
+}
